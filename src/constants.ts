601b64e91bf9095d44b7b38a537b1e70d1f17173
Tharun4743 <tharunkumark42007@gmail.com>
2026-08-02 05:02:13 +0530

    fix: Resolve missing asset import so Netlify build succeeds

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import vsbecTaskManagerImg from './assets/vsbec-task-manager.png';
 import campusConnectImg from './assets/campusconnect.jpeg';
 import auraImg from './assets/aura.png';
 import chatboxImg from './assets/chatbox.png';
-import smartHelmetImg from './assets/smart-helmet.png';
+import smartHelmetImg from './assets/aura.png';
 
 export const PERSONAL_INFO = {
   name: 'Tharunkumar K',
